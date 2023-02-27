0fcb6f4eaa38d3c60e67acc0115e6a362a46fe46
Emily Canto <56757680+dronade@users.noreply.github.com>
2023-02-27 00:03:30 +0000

    added create task form

--- src/main/java/dev/dronade/taskorca/controller/AddTasksController.java
+++ src/main/java/dev/dronade/taskorca/controller/AddTasksController.java
@@ -1,9 +1,14 @@
 package dev.dronade.taskorca.controller;
 
+import dev.dronade.taskorca.TaskOrcaApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class AddTasksController {
 
@@ -11,12 +16,24 @@ public class AddTasksController {
     private ImageView addTasksButton;
 
     @FXML
+    private AnchorPane GreyBox;
+
+    @FXML
     private Label NoTasksLabel;
 
     @FXML
     void initialize() {
         addTasksButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            addTasksButton.setOpacity(0);
+            NoTasksLabel.setOpacity(0);
 
+            try {
+                AnchorPane AddTaskPane =
+                        FXMLLoader.load((TaskOrcaApplication.class.getResource("AddTasksFormView.fxml")));
+                GreyBox.getChildren().setAll(AddTaskPane);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         });
     }
 }
--- src/main/java/dev/dronade/taskorca/controller/AddTasksFormController.java
+++ src/main/java/dev/dronade/taskorca/controller/AddTasksFormController.java
@@ -1,4 +1,37 @@
 package dev.dronade.taskorca.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddTasksFormController {
+
+    @FXML
+    private TextField TaskTitleInput;
+
+    @FXML
+    private TextField AdditionalDetailsInput;
+
+    @FXML
+    private DatePicker DueDateInput;
+
+    @FXML
+    private Label TaskTitleLabel;
+
+    @FXML
+    private Label AdditionalDetailsLabel;
+
+    @FXML
+    private Label DueDateLabel;
+
+    @FXML
+    private Button CreateTaskButton;
+
+
+    @FXML
+    void initialize() {
+
+    }
 }
