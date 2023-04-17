a2bad3c394732252f81a36312af25ac8a0c15ad1
Emily Canto <56757680+dronade@users.noreply.github.com>
2023-04-18 00:04:30 +0100

    drag and drop finally works
    + added a handle for cool ux design 😎

--- src/main/java/dev/dronade/taskorca/controller/TaskCellController.java
+++ src/main/java/dev/dronade/taskorca/controller/TaskCellController.java
@@ -3,11 +3,16 @@ package dev.dronade.taskorca.controller;
 import dev.dronade.taskorca.TaskOrcaApplication;
 import dev.dronade.taskorca.database.TaskDatabase;
 import dev.dronade.taskorca.model.Task;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -44,6 +49,73 @@ public class TaskCellController extends ListCell<Task> {
     private FXMLLoader fxmlLoader;
     private TaskDatabase db;
 
+    public TaskCellController() {
+        setOnDragDetected(event -> {
+            if (getItem() == null) {
+                return;
+            }
+
+            ObservableList<Task> items = getListView().getItems();
+
+            int index = items.indexOf(getItem());
+
+            Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(String.valueOf(index));
+            dragboard.setContent(content);
+
+            event.consume();
+        });
+
+        setOnDragOver(event -> {
+            if (event.getGestureSource() != this &&
+                    event.getDragboard().hasString()) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
+
+            event.consume();
+        });
+
+        setOnDragEntered(event -> {
+            if (event.getGestureSource() != this &&
+                    event.getDragboard().hasString()) {
+                setOpacity(0.3);
+            }
+        });
+
+        setOnDragExited(event -> {
+            if (event.getGestureSource() != this &&
+                    event.getDragboard().hasString()) {
+                setOpacity(1);
+            }
+        });
+
+        setOnDragDropped(event -> {
+            if (getItem() == null) {
+                return;
+            }
+
+            Dragboard dragboard = event.getDragboard();
+            boolean success = false;
+
+            if (dragboard.hasString()) {
+                ObservableList<Task> items = getListView().getItems();
+                int draggedIdx = Integer.parseInt(dragboard.getString());
+                int thisIdx = items.indexOf(getItem());
+
+                Task temp = items.get(draggedIdx);
+                items.set(draggedIdx, items.get(thisIdx));
+                items.set(thisIdx, temp);
+
+                success = true;
+            }
+            event.setDropCompleted(success);
+
+            event.consume();
+        });
+
+        setOnDragDone(DragEvent::consume);
+    }
 
     @Override
     public void updateItem(Task task, boolean empty) {
